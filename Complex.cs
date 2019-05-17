78116331671997a89a115cd323e38551a9610ee2
sevtapberan <sevtapberan@gmail.com>
2019-05-18 02:25:17 +0300

    Complex sayı işlemleri

---
+++ Complex.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NYPL24022017
+{
+    class Complex
+    {
+        double re;
+        double im;
+        
+
+        public Complex(double re, double im)
+        {
+            Re = re;
+            Im = im;
+        }
+
+        public Complex()
+        { }
+
+        public double Re
+        {
+            get
+            {
+                return re;
+            }
+
+            set
+            {
+                re = value;
+            }
+        }
+
+        public double Im
+        {
+            get
+            {
+                return im;
+            }
+
+            set
+            {
+                im = value;
+            }
+        }
+
+
+        public static Complex ComplexFromCartesian(double re, double im)
+        {
+            return new Complex(re, im);
+        }
+        public static Complex ComplexFromPolar(double dist, double angle)
+        {
+            double re = dist * Math.Cos(Math.PI/(180.0/angle));
+            double im = dist * Math.Sin(Math.PI / (180.0 / angle));
+            return new Complex(re,im);
+        }
+
+        public void Add(Complex num2)
+        {
+            Re += num2.Re;
+            Im += num2.Im;
+        }
+
+        public void Substract(Complex num2)
+        {
+            Re -= num2.Re;
+            Im -= num2.Im;
+        }
+
+        public void Multiply(Complex num2)
+        {
+            double tempRe = Re * num2.Re - Im * num2.Im;
+            Im = Re * num2.Im + Im * num2.Re;
+            Re = tempRe;
+        }
+
+        public static Complex Multiply(Complex num1, Complex num2)
+        {
+            double tempRe = num1.Re * num2.Re - num1.Im * num2.Im;
+            return new Complex(num1.Re * num2.Re - num1.Im * num2.Im,
+               num1.Re * num2.Im + num1.Im * num2.Re);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NYPL24022017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c1 = new Complex(3, 3);
+            Complex c2 = new Complex();
+            Console.WriteLine(c1.Re+"+"+c1.Im+"i");
+            Console.WriteLine(c2.Re + "+" + c2.Im+"i");
+            Complex c3 = Complex.ComplexFromCartesian(3,5);
+            Complex c4 = Complex.ComplexFromPolar(3*Math.Sqrt(2),45);
+            Console.WriteLine(c4.Re + "+" + c4.Im + "i");
+
+            c1.Add(c4);
+            Console.WriteLine(c1.Re + "+" + c1.Im + "i");
+            c4.Add(c1);
+            Console.WriteLine(c4.Re + "+" + c4.Im + "i");
+           // c1.Multiply(c3);//carpim sonucu c1in uzerine yazilir.
+            Complex c5 = Complex.Multiply(c1,c3); // carpim sonucu olarak yeni bir complex sayi dondurulur
+        }
+    }
+}
